911f4f377ae1baf91fb00ff49f3f2af23d1c13f0
Kai-Chun Yang <32695855+kai-chun@users.noreply.github.com>
2018-01-02 21:15:53 +0800

    Update PlayerCtrl.cs

--- Assets/PlayerCtrl.cs
+++ Assets/PlayerCtrl.cs
@@ -4,43 +4,41 @@ using UnityEngine;
 
 public class PlayerCtrl : MonoBehaviour {
 
-	//float speed;
 	public GameObject bullet;
 	public int HP;
 
 	// Use this for initialization
 	void Start () {
-		//speed = 15;
 		HP = 4;
 	}
 
-	public void OnHit(){
+	public void OnHit () {
 		HP--;
-		if (HP <= 0) {
+		if (HP <= 0) { // 若 player 的 HP 小於 0 則摧毀 player
 			Destroy (this.gameObject);
 		}
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		if(Input.GetKey(KeyCode.W)){			
-			this.transform.Translate (Vector3.up*Time.deltaTime*5);//move right for 5 times/per second
+		if (Input.GetKey(KeyCode.W)){			
+			this.transform.Translate (Vector3.up*Time.deltaTime*5); //move right for 5 times/per second
 		}
-		if(Input.GetKey(KeyCode.S)){			
-			this.transform.Translate (Vector3.down*Time.deltaTime*5);//move right for 5 times/per second
+		if (Input.GetKey(KeyCode.S)){			
+			this.transform.Translate (Vector3.down*Time.deltaTime*5); //move right for 5 times/per second
 		}
-		if(Input.GetKey(KeyCode.A)){			
-			this.transform.Translate (Vector3.left*Time.deltaTime*5);//move right for 5 times/per second
+		if (Input.GetKey(KeyCode.A)){			
+			this.transform.Translate (Vector3.left*Time.deltaTime*5); //move right for 5 times/per second
 		}
-		if(Input.GetKey(KeyCode.D)){			
-			this.transform.Translate (Vector3.right*Time.deltaTime*5);//move right for 5 times/per second
+		if (Input.GetKey(KeyCode.D)){			
+			this.transform.Translate (Vector3.right*Time.deltaTime*5); //move right for 5 times/per second
 		}
-		if(Input.GetKeyDown(KeyCode.Space)){
+		if (Input.GetKeyDown(KeyCode.Space)){ // 空白鍵發射子彈
 			GameObject copyObj = GameObject.Instantiate (bullet, this.transform.position, bullet.transform.rotation);
 			copyObj.transform.Rotate (0, 0, -90);
 			copyObj.tag = this.tag;
 		}
-
+		// 避免 player 跑出視窗
 		if (this.transform.position.x >= Camera.main.aspect * Camera.main.orthographicSize) { //orthographicSize為Camera的尺寸，因x較寬 所以須乘上aspect(Camera的長寬比，長:寬=1:?)
 			Vector3 pos=this.transform.position;
 			pos.x = Camera.main.aspect * Camera.main.orthographicSize;
@@ -61,5 +59,5 @@ public class PlayerCtrl : MonoBehaviour {
 			pos.y = -Camera.main.orthographicSize;
 			this.transform.position = pos;
 		}
-}
+	}
 }
